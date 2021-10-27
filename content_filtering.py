674222502740159aa577d4180af9ca89378e2c8e
DavidGu <35072445+ChengWeiGu@users.noreply.github.com>
2021-10-27 17:54:59 +0800

    update on 10/27

---
+++ content_filtering.py
@@ -0,0 +1,85 @@
+# coding=utf-8
+# https://github.com/nikitaa30/Content-based-Recommender-System/blob/master/recommender_system.py
+import pandas as pd
+import numpy as np
+import json
+import jieba
+from os import listdir
+from os.path import join, basename, dirname
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+import time
+import datetime
+from preprocess import generate_stop_words, tokenize
+
+
+def preprocess_query(input_str):
+    
+    stop_word_list=generate_stop_words()
+    sent_words = tokenize(input_str,stop_word_list)
+    document = " ".join(sent_words)
+    
+    return document
+        
+
+
+# to rank restaurant by query
+def recommend_restaurants(input_str="牛肉麵", top_n=10):
+    
+    def get_item_name(id):
+        return df.loc[df['id'] == id]['name'].tolist()[0]
+    
+    def get_item_descr(id):
+        return df.loc[df['id'] == id]['description_orig'].tolist()[0]
+        # return df.loc[df['id'] == id]['description'].tolist()[0]
+        
+    def get_item_url(id):
+        return df.loc[df['id'] == id]['url'].tolist()[0]
+    
+    
+    def recommend(input_str, top_n):
+        res_list=[]
+        print("Recommending top " + str(top_n) + " products based on query: " + input_str)
+        print("="*160)
+        for order, rec in enumerate(similar_items,1):
+            res_list+=[(rec[1], rec[0], get_item_url(rec[1]))] # element: (id,score,url)
+            print("Recommended%d: "%order + get_item_name(rec[1]) + " (score:" + str(np.around(rec[0],6)) + ")" + " /description: " + get_item_descr(rec[1]))
+            print("-"*160)
+        
+        return res_list
+    
+    
+    df = pd.read_csv('tripadvisor_data_henry.csv',encoding = 'utf-8-sig')
+    
+    # tf = TfidfVectorizer(analyzer='word', max_features=200 , ngram_range=(1, 1), min_df=0, max_df = 0.6,stop_words=["是","的","了"],token_pattern=r"(?u)\b\w+\b").fit(df['description'])
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, max_df = 1.0,stop_words=["是","的","了"],token_pattern=r"(?u)\b\w+\b").fit(df['description'])
+    
+    tfidf_matrix = tf.fit_transform(df['description'])
+    
+    input_str_pre = preprocess_query(input_str)
+    time_now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S_data')
+    df_input = pd.DataFrame({'datetime':[time_now],'description':[input_str], 'description_preprocess':[input_str_pre]})
+    # print('the query info:\n',df_input)
+    # print('\n')
+    
+    
+    tfidf_matrix_input = tf.transform(df_input['description_preprocess'])
+    cosine_similarities = linear_kernel(tfidf_matrix_input, tfidf_matrix)
+
+    
+    similar_indices = cosine_similarities[0].argsort()[:-(top_n+1):-1] # top-n : the most similar
+    similar_items = [(cosine_similarities[0][i], df['id'][i]) for i in similar_indices]
+    
+    
+    return input_str, input_str_pre, recommend(input_str = input_str, top_n=top_n)
+    
+
+
+if __name__ == "__main__":
+    input_str = input("find your favorite restaurant! input query:  ") # do query: e.g. 牛肉麵 or 燒肉烤肉CP值高 or 火鍋吃到飽 or 壽司...
+    query, query_pre, res_list = recommend_restaurants(input_str = input_str, top_n = 20)
+    print("-"*80,"Summary","-"*80)
+    print("The input query: {0}\nThe preprocessed query: {1}".format(query, query_pre))
+    print(f"the recommended restaurants from top-1 to top-{len(res_list)}:\n\n",res_list)
+    print("-"*80,"End","-"*80)
+    
---
+++ preprocess.py
@@ -0,0 +1,97 @@
+# coding=utf-8
+import pandas as pd
+import numpy as np
+import json
+import jieba
+from os import listdir
+from os.path import join, basename, dirname
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+import time
+import datetime
+
+
+def read_data():
+    data = []
+    with open('tripadvisor_restaurant_formatted_henry.json', 'r',encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+    
+
+def generate_stop_words():
+    stop_word_list = []
+    with open("stopwords1893_cn.txt",'r', encoding = 'utf-8-sig') as f:
+        stop_word_list = [line.strip('\n') for line in f.readlines()]
+        # print(stop_word_list)
+        f.close()
+    return stop_word_list
+
+
+def tokenize(text, stop_word_list):
+    
+    cut_words = list(jieba.cut(text))
+    
+    new_cut_words = []
+    for word in cut_words:
+        if word not in stop_word_list:
+            new_cut_words.append(word)
+        else:
+            pass
+            
+    return new_cut_words
+
+
+
+def json2csv():
+
+    data = read_data()
+    stop_word_list=generate_stop_words()
+    
+    id_list = []
+    url_list = []
+    name_list = []
+    description_orig_list = []
+    description_final_list = []
+    
+    total_doc = len(data)
+    
+    count = 0
+    for _id, restaurant_data in enumerate(data,1):
+        count += 1
+        print(f'progress:{count}/{total_doc}={np.around(count/total_doc*100,2)}%')
+        
+        id_list += [restaurant_data['asin']]
+        url_list += [restaurant_data['url']]
+        name_list += [restaurant_data['name']]
+        
+        str1 = restaurant_data['name'] + '，' #21工房天然手工涼麵（酒泉店，
+        
+        
+        str2 = ""
+        criteria = 20
+        for review in restaurant_data['reviews']:
+            if len(review['quote']) < criteria:
+                str2 += (review['quote'] + '，') # 老字號西餐館, CP值超低, 好吃...
+            else:
+                pass
+        
+        
+        str3 = restaurant_data['rating']['overall']+ '，' #4.0，
+        str4 = ' '.join(str(e) for e in restaurant_data['characteristics']) + '，' #菜系 日式料理 亞洲料理 餐點 午餐, 晚餐，
+        text = str1+str2+str3+str4
+        description_orig_list += [text]
+        
+        
+        sent_words = tokenize(text,stop_word_list)
+        document = " ".join(sent_words)
+        description_final_list += [document]
+        
+        
+        
+    pd.DataFrame({'id':id_list, 'name':name_list, 'url':url_list, 'description_orig':description_orig_list, 'description':description_final_list}).to_csv('tripadvisor_data_henry.csv',index = False, encoding='utf-8-sig')    
+
+
+
+if __name__ == "__main__":
+    json2csv()
+    
